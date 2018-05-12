461a71522664cfc3c174665f3095a0d1d1954576
TexnoMann <ElectroDans@yandex.ru>
2018-05-12 21:32:41 +0300

    fixed addColumn

--- Matrix.py
+++ Matrix.py
@@ -1,3 +1,4 @@
+import copy
 class Matrix():
 	def __init__(self, lists):
 		assert(lists), "This is empty list from initialization matrix"
@@ -17,8 +18,8 @@ class Matrix():
 
 	def __str__(self):
 		stringMatrix=""
-		for i in range(self.__rowCount):
-			for j in range(self.__columnCount):
+		for i in range(len(self.__matrix)):
+			for j in range(len(self.__matrix[0])):
 				stringMatrix+=str(self.__matrix[i][j]) + " "
 			stringMatrix+=" \n"
 		return stringMatrix
@@ -36,7 +37,7 @@ class Matrix():
 
 
 	def __scalarMul(self,other):
-		return Matrix(list(map(lambda x: list(map(lambda y: y*other ,x)), self.__matrix)))
+		return Matrix(list(map(lambda x: list(map(lambda y: y*other ,x)), self.__matrix)).copy())
 
 	def __matrixMul(self,other):
 		__mat=[]
@@ -51,6 +52,24 @@ class Matrix():
 			__mat.append(__rowM)
 		return Matrix(__mat)
 
+	def getInverseMatrix(self):
+		temp=Matrix(self.__matrix.copy())
+		initEiMatrix=[]
+		for i in range (self.__rowCount):
+			row=[0]*self.__rowCount
+			initEiMatrix.append(row)
+		for i in range(self.__rowCount): 
+			initEiMatrix[i][i]=1
+		temp.__addExtendetMatrix(Matrix(initEiMatrix))
+		matrixDiagonalList=temp.getDiagonalEqualsMatrix().getMatrixInList()
+		invertMatrix=[]
+		for i in range(self.__rowCount):
+			assert( matrixDiagonalList[i][i]!=0),"Determinant Matrix is zero"
+			rows=list(map(lambda x: x/matrixDiagonalList[i][i],matrixDiagonalList[i]))
+			invertMatrix.append(rows[self.__rowCount:])
+
+		return Matrix(invertMatrix)
+
 
 	def __pow__(self, N):
 		temp=self
@@ -59,13 +78,20 @@ class Matrix():
 
 
 	def getMatrixInList(self):
-		return self.__matrix
+		newmatrix=copy.deepcopy(self.__matrix)
+		return newmatrix
 
 	def addRow(self, matrixRow):
 		self.__matrix.append(matrixRow.__matrix[0])
 		self.__rowCount+=1
 
+	def __addExtendetMatrix(self, matrixEx):
+		assert(self.__rowCount == matrixEx.getRowCount()),"Error Dimension!"
+		list(map(lambda x, y: list(map(lambda z:x.append(z),y)),self.__matrix, matrixEx.__matrix))
+		self.__columnCount+=matrixEx.__columnCount
+
 	def addColumn(self,matrixCollumn):
+		assert(self.__rowCount == matrixCollumn.__rowCount), "Error Dimension!"
 		list(map(lambda x, y: x.append(y[0]),self.__matrix, matrixCollumn.__matrix))
 		self.__columnCount+=1
 
@@ -85,26 +111,38 @@ class Matrix():
 	def getCollumnCount(self):
 		return self.__columnCount
 
-	def __getDiagonalEqualsMatrix(self):
+	def getDiagonalEqualsMatrix(self):
 		matrixDiagonal=self.__matrix.copy()
-		assert(self.__columnCount == self.__rowCount), "Dimension Error"
+		assert((self.__columnCount == self.__rowCount) or(2*self.__rowCount == self.__columnCount)), "Dimension Error"
 
-		for i in range(self.__columnCount):
+		for i in range(self.__rowCount):
 			for j in range(self.__rowCount):
 				temprow=[]
 				if i !=j  and matrixDiagonal[i][i]!=0 and matrixDiagonal[j][i]!=0:
 					temprow= list(map(lambda x: x/matrixDiagonal[i][i]*(-1)*matrixDiagonal[j][i],matrixDiagonal[i]))
 					matrixDiagonal[j]=list(map(lambda x, y: x+y, temprow, matrixDiagonal[j]))
-		return matrixDiagonal;
+		return Matrix(matrixDiagonal);
+
+		# def __getDiagonalEqualsMatrix(self):
+		# matrixDiagonal=self.__matrix.copy()
+		# assert((self.__columnCount == self.__rowCount) or(2*self.__rowCount == self.__columnCount)), "Dimension Error"
+
+		# for i in range(self.__columnCount):
+		# 	for j in range(self.__rowCount):
+		# 		temprow=[]
+		# 		if i !=j  and matrixDiagonal[i][i]!=0 and matrixDiagonal[j][i]!=0:
+		# 			temprow= list(map(lambda x: x/matrixDiagonal[i][i]*(-1)*matrixDiagonal[j][i],matrixDiagonal[i]))
+		# 			matrixDiagonal[j]=list(map(lambda x, y: x+y, temprow, matrixDiagonal[j]))
+		# return matrixDiagonal;
 
 	def getDeterminant(self):
-		matrixDiagonal=self.__getDiagonalEqualsMatrix()	
+		matrixDiagonal=self.getDiagonalEqualsMatrix().__matrix.copy()	
 		determinant=1.0
 		for i in range(self.__rowCount): determinant*=matrixDiagonal[i][i]
 		return round(determinant,5)
 
 	def getRang(self):
-		matrixDiagonal=self.__getDiagonalEqualsMatrix()	
+		matrixDiagonal=self.getDiagonalEqualsMatrix().__matrix.copy()
 		rang = 0
 		for i in range(self.__rowCount):
 			countZero=0
